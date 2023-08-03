3c82fc8752235a0dc8c8eb70440ce48c884c7a84
hasanhttps <hasanabdullazad@gmail.com>
2023-08-03 18:23:47 +0400

    State Pattern

---
+++ State Pattern Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace State_Pattern_Homework;
+
+interface IState { // State changes when Do function called
+    void Do(IDevice device);
+}
+
+// This interface is not about State Pattern it is additional principe for open closed principle
+interface IDevice {
+    public IState State { get; set; }
+    void PowerButton();
+}
+
+class Computer : IDevice {
+
+    public IState State { get; set; }
+
+    public Computer() {
+        State = new OffState();
+    }
+    public void PowerButton() 
+        => State.Do(this);
+}
+
+class OffState : IState {
+    public void Do(IDevice device) {
+        Console.WriteLine("Cihaz açıldı");
+        device.State = new OnState();
+    }
+}
+
+class OnState : IState { 
+    public void Do(IDevice device) {
+        Console.WriteLine("Cihaz bağlandı");
+        device.State = new OffState();
+    }
+}
+
+internal class Program {
+    static void Main(string[] args) { 
+        IDevice device = new Computer();
+        device.PowerButton();
+        device.PowerButton();
+        device.PowerButton();
+        device.PowerButton();
+
+    }
+}
